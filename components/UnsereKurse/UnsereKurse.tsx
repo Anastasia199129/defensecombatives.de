d3b7b5c8520a14be21e76c69902223ff9f2f21cc
Anastasia199129 <nastasia199126@gmail.com>
2023-06-30 00:47:44 +0200

    <Image/>

--- components/UnsereKurse/UnsereKurse.tsx
+++ components/UnsereKurse/UnsereKurse.tsx
@@ -1,27 +1,47 @@
-import data from './data.json'
+import Image from 'next/image'
 
 import Container from '../Container/Container'
+import Title from '../Title/Title'
+import Link from 'next/link'
+import Arrow from '../SpezAngebote/Arrow'
+
+import data from './data.json'
 
 import s from './UnsereKurse.module.sass'
-import Title from '../Title/Title'
-
 
 export default function UnsereKurse() {
-  return <Container>
-    <section>
-      <Title title='unsere kurse'/>
-    <ul className={s.wrapper}>
-      {data.map(({id, text, img}) => (
-        <li key={id}>
-         <img src={img.src} alt={img.alt} />
-           <div
-              dangerouslySetInnerHTML={{
-                __html: text
-              }}
-            />
-          </li>
-      ))}
-    </ul>
-    </section>
-  </Container>
+  return (
+    <Container>
+      <section>
+        <Title title='unsere kurse' />
+        <ul className={s.wrapper}>
+          {data.map(({ id, text = '', link = {}, img }) => (
+            <li className={`${link.link && s.itemWithLink}`} key={id}>
+              {/* <img src={img.src} alt={img.alt} /> */}
+              <Image
+                layout='responsive'
+                width={1000}
+                height={120}
+                src={img.src}
+                alt={img.alt}
+              />
+              {text && (
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: text,
+                  }}
+                />
+              )}
+              {link.text && link.link && (
+                <Link className={s.link} href={link.link}>
+                  {link.text}
+                  <Arrow />
+                </Link>
+              )}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Container>
+  )
 }
